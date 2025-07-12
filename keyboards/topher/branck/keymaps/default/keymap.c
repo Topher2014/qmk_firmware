05f690b20bf905c35dc38bd151e7f476127f1868
Topher Ludlow <topherludlow@protonmail.com>
2025-07-11 22:13:15 -0700

    Fixed merge conflicts.

--- keyboards/topher/branck/keymaps/default/keymap.c
+++ keyboards/topher/branck/keymaps/default/keymap.c
@@ -16,10 +16,6 @@
 
 #include QMK_KEYBOARD_H
 
-#ifdef AUDIO_ENABLE
-#    include "muse.h"
-#endif
-
 enum planck_layers {
   _QWERTY,
   _LOW,
@@ -42,10 +38,6 @@ enum tap_dance_keycodes {
     K_UP,
     L_RGHT,
     P_PSCR,
-<<<<<<< Updated upstream
-    BTN1_BTN2,
-=======
->>>>>>> Stashed changes
 };
 
 #define LOW MO(_LOW)
@@ -53,46 +45,6 @@ enum tap_dance_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-<<<<<<< Updated upstream
-[_QWERTY] = LAYOUT(
-    TD(Q_TAB), KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, TD(P_PSCR), 
-    LGUI_T(KC_A), KC_S, KC_D, KC_F, RCTL_T(KC_G), TD(H_LEFT), TD(J_DOWN), TD(K_UP), TD(L_RGHT), KC_BSPC,
-    RALT_T(KC_Z), KC_X, KC_C, KC_V, LSFT_T(KC_B), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, 
-    LOWER, LSFT_T(KC_ENT), _______, KC_INS, LCTL_T(KC_SPC), LOW
-),
-
-[_LOW] = LAYOUT(
-    KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, 
-    QW, _______, KC_LCBR, KC_RCBR, KC_COLN, KC_GRV, KC_MINS, KC_PLUS, KC_PIPE, KC_DEL,
-    _______, _______, _______, _______, _______, _______, KC_QUOT, _______, _______, KC_BSLS,
-    _______, _______, _______, TG(_GAME), _______, _______ 
-),
-
-[_LOWER] = LAYOUT(
-    KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, 
-    KC_ESC, _______, KC_LBRC, KC_RBRC, KC_SCLN, _______, KC_UNDS, KC_EQL, _______, _______, 
-    _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, _______, _______, 
-    _______, _______, _______, _______, _______, _______ 
-),
-
-[_LOWEST] = LAYOUT(
-    KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, 
-    KC_F11, KC_F12, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, 
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
-    _______, QK_RBT, QK_BOOT, _______, _______, _______ 
-),
-
-[_GAME] = LAYOUT(
-    KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
-    KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_BSPC,
-    KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_RSFT, KC_TAB, KC_ESC, 
-    KC_J, KC_K, KC_L, TG(_GAME), KC_SPC, KC_ENT
-)
-
-};
-
-// Tap Dance definitions
-=======
 [_QWERTY] = LAYOUT_ortho_4x12(
     TD(Q_TAB), KC_W, KC_E, KC_R, KC_T, _______, _______, KC_Y, KC_U, KC_I, KC_O, TD(P_PSCR), 
     LGUI_T(KC_A), KC_S, KC_D, KC_F, RCTL_T(KC_G), _______, _______, TD(H_LEFT), TD(J_DOWN), TD(K_UP), TD(L_RGHT), KC_BSPC, 
@@ -131,7 +83,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
->>>>>>> Stashed changes
 tap_dance_action_t tap_dance_actions[] = {
     [Q_TAB] = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_TAB),
     [H_LEFT] = ACTION_TAP_DANCE_DOUBLE(KC_H, KC_LEFT),
@@ -140,25 +91,14 @@ tap_dance_action_t tap_dance_actions[] = {
     [L_RGHT] = ACTION_TAP_DANCE_DOUBLE(KC_L, KC_RGHT),
     [M_QUOT] = ACTION_TAP_DANCE_DOUBLE(KC_M, KC_QUOT),
     [P_PSCR] = ACTION_TAP_DANCE_DOUBLE(KC_P, KC_PSCR),
-<<<<<<< Updated upstream
-    [BTN1_BTN2] = ACTION_TAP_DANCE_DOUBLE(KC_BTN1, KC_BTN2),
 };
 
-// Custom tapping term per key
-=======
-};
-
->>>>>>> Stashed changes
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case TD(H_LEFT): return 160;
         case TD(J_DOWN): return 150;
         case TD(K_UP): return 160;
-<<<<<<< Updated upstream
-        case TD(L_RGHT): return 115;
-=======
         case TD(L_RGHT): return 100;
->>>>>>> Stashed changes
         case TD(M_QUOT): return 140;
         case TD(Q_TAB): return 210;
         case TD(P_PSCR): return 130;
@@ -196,7 +136,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 }
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
